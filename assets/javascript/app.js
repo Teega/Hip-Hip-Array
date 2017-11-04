4c2cf85161a191c1d76819eede6a7168050f41c7
maggiebarker <maggieb.06@gmail.com>
2017-11-04 11:23:13 -0600

    resetting maggie branch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,25 @@
+
+
+
+$( document ).ready(function() {
+        $('#news-button').on("click", function(){
+          console.log("News is working")
+        });
+        $('#movies-button').on("click", function(){
+          console.log("Movies is working")
+        });
+        $('#music-button').on("click", function(){
+          console.log("Music is working")
+        });
+        $('#youtube-button').on("click", function(){
+          console.log("Youtube is working")
+        });
+    });
+
 console.log("this is a test");
 
 
 //movies
-
 //imdb apikey = f8e63df97dfc2a7095345babf9d3fe54
 
 var settings = {
@@ -24,12 +41,12 @@ $.ajax(settings).done(function(response) {
 
   for (var i = 0; i < result.length; i++) {
            if (result[i].genre_ids[0] !== 99 && result[i].original_language == "en") {
-           	console.log("hey we are in our if");
-           	var movieDiv = $("#movie");
-           	var title = result[i].original_title;
-           	console.log(title);
-           	movieDiv.append(title + "<hr>");
-		};
-	};
+            console.log("hey we are in our if");
+            var movieDiv = $("#movie");
+            var title = result[i].original_title;
+            console.log(title);
+            movieDiv.append(title + "<hr>");
+    };
+  };
 
-})	
+})  
