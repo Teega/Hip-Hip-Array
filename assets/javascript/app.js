a6297974497f95222537083167aca9fca644a613
maggiebarker <maggieb.06@gmail.com>
2017-11-04 12:37:46 -0600

    add parameter to movie search, extracted url to be able to put date into it

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,44 +21,37 @@ console.log("this is a test");
 
 //movies
 //imdb apikey = f8e63df97dfc2a7095345babf9d3fe54
+// /3/discover/movie?primary_release_year=2010&sort_by=vote_average.desc
+
+var year = 1992;
+var movieURL = "https://api.themoviedb.org//3/discover/movie?primary_release_year="+ year +"&sort_by=vote_average.descpage=2&language=en-US&api_key=f8e63df97dfc2a7095345babf9d3fe54";
+
 
 var settings = {
   "async": true,
   "crossDomain": true,
-  "url": "https://api.themoviedb.org//3/discover/movie?primary_release_year=1998&sort_by=vote_average.descpage=1&language=en-US&api_key=f8e63df97dfc2a7095345babf9d3fe54",
+  "url": movieURL,
   "method": "GET",
   "headers": {},
-  "data": "{}"
+  "data": "{}",
 }
 
-// /3/discover/movie?primary_release_year=2010&sort_by=vote_average.desc
+// var year = $("#year-addon");
 
 $.ajax(settings).done(function(response) {
   console.log(response);
-//  $("#movie").text(JSON.stringify(response));
+ $("#movie").text(JSON.stringify(response));
 
  var result = response.results;
 
   for (var i = 0; i < result.length; i++) {
-           if (result[i].genre_ids[0] !== 99 && result[i].original_language == "en") {
-<<<<<<< HEAD
-            console.log("hey we are in our if");
-            var movieDiv = $("#movie");
-            var title = result[i].original_title;
-            console.log(title);
-            movieDiv.append(title + "<hr>");
-    };
-  };
-
-})  
-=======
-           	console.log("hey we are in our if");
-           	var movieDiv = $("#movie");
+           if (result[i].genre_ids[0] !== 99 && result[i].original_language == "en" && result[i].popularity > 5) {
+           	var movieDiv = $(".moviesResults");
            	var title = result[i].original_title;
            	console.log(title);
            	movieDiv.append(title + "<hr>");
 		};
 	};
 
-})	
->>>>>>> master
+})
+
