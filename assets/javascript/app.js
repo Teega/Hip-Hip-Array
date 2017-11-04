b876bcdab24b35ddcb407f7c9f0c751c32e84b12
maggiebarker <maggieb.06@gmail.com>
2017-11-04 16:42:50 -0600

    posters, plots and titles going to the DOM

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,7 +23,7 @@ console.log("this is a test");
 // /3/discover/movie?primary_release_year=2010&sort_by=vote_average.desc
 
 // var year = $("#year-addon");
-var year = 2007;
+var year = 2009;
 var movieURL = "https://api.themoviedb.org//3/discover/movie?primary_release_year="+ year +"&sort_by=vote_average.descpage=2&language=en-US&api_key=f8e63df97dfc2a7095345babf9d3fe54";
 
 var settings = {
@@ -37,18 +37,28 @@ var settings = {
 
 $.ajax(settings).done(function(response) {
   console.log(response);
-// $("#movie").text(JSON.stringify(response));
 
- var result = response.results;
+var result = response.results;
 
   for (var i = 0; i < result.length; i++) {
-           if (result[i].genre_ids[0] !== 99 && result[i].original_language == "en" && result[i].popularity > 5) {
-           	var movieDiv = $(".moviesResults");
+           if (result[i].genre_ids[0] !== 99 && result[i].original_language == "en" && result[i].popularity >= 15) {
+           	var movieDiv = $("#moviesResults");
            	var title = result[i].original_title;
-           	console.log(title);
-           	movieDiv.append("<br>" + title + "<hr>");
+              console.log(title);
+            var plot = result[i].overview;
+              console.log(plot);
+            var movieIMG = result[i].poster_path;
+            var imgURL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2" + movieIMG;
+              console.log(imgURL);
+            var image = $("<img>").attr("src", imgURL);
+            movieDiv.append(image);
+           	movieDiv.append("<h4>" + title + "</h4><p>" + plot + "</p><hr>");
+
 		};
 	};
 
 })
+
+//https://image.tmdb.org/t/p/w185_and_h278_bestv2/eyYqrggNEJpoqulbyaDBW2ABjwO.jpg
+
 
