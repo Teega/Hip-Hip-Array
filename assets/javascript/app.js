c14fd62491fa2dccdbd56404b154ff6cfc19514a
maggiebarker <maggieb.06@gmail.com>
2017-11-04 19:23:21 -0600

    got tvshows from the same api, put in the DOM, still need to figure out how to search by date

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,3 @@
-
-
-
 $( document ).ready(function() {
         $('#news-button').on("click", function(){
         	console.log("News is working")
@@ -16,8 +13,6 @@ $( document ).ready(function() {
         });
     });
 
-console.log("this is a test");
-
 //movies:
 //imdb apikey = f8e63df97dfc2a7095345babf9d3fe54
 // /3/discover/movie?primary_release_year=2010&sort_by=vote_average.desc
@@ -25,7 +20,7 @@ console.log("this is a test");
 //Remember that we can change the year/page with user input or onclick events, ie: $("#seemore").on("click", function(page++));
 // var year = $("#year-addon");
 var year = 2009;
-var page = 10;
+var page = 3;
 var movieURL = "https://api.themoviedb.org//3/discover/movie?primary_release_year="+ year +"&sort_by=vote_average.descpage=" + page + "&language=en-US&api_key=f8e63df97dfc2a7095345babf9d3fe54";
 
 //This bit came from IMDB, don't mess with it, we need it to pull data correctly
@@ -39,23 +34,20 @@ var settings = {
 }
 
 $.ajax(settings).done(function(response) {
-  console.log(response);
+  console.log(response, "These are movies");
 
 //Establish a variable that pulls results out of the response
 var result = response.results;
 
 //Loop through the results, isolate most popular/appropriate movies, establish variables to be able to refer to specific results later
   for (var i = 0; i < result.length; i++) {
-           if (result[i].genre_ids[0] !== 99 && result[i].original_language == "en" && result[i].popularity >= 5 && result[i].vote_average >=6) {
+          if (result[i].genre_ids[0] !== 99 && result[i].original_language == "en" && result[i].popularity >= 5 && result[i].vote_average >=6) {
            	var movieDiv = $("#moviesResults");
             var imgDiv = $("#imgDiv");
            	var title = result[i].original_title;
-              console.log(title);
             var plot = result[i].overview;
-              console.log(plot);
             var movieIMG = result[i].poster_path;
             var imgURL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2" + movieIMG;
-              console.log(imgURL);
 //            var image = $("<img>").attr("src", imgURL);
 
 //Grab the table from the HTML and put our movie data in it!  
@@ -66,14 +58,55 @@ var result = response.results;
               var descCell = row.insertCell(1);
 
                 posterCell.innerHTML = ("<img src=" + imgURL + ">");
-                descCell.innerHTML = ("<h4>" + title + "</h4><p>" + plot + "</p><hr>");
+                descCell.innerHTML = ("<h3>" + title + "</h3><p>" + plot + "</p><hr>");
 
-		};
+		      };
 	};
-
-
 
 })
 
+//tv shows
+//imdb apikey = f8e63df97dfc2a7095345babf9d3fe54
+//
 
+var tvYear = 1999;
+var page = 1;
+var tvURL = "https://api.themoviedb.org//3/discover/tv?primary_release_year=" + tvYear + "&language=en-US&api_key=f8e63df97dfc2a7095345babf9d3fe54";
 
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url":  tvURL,
+  "method": "GET",
+  "headers": {},
+  "data": "{}"
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response, "These are TV Shows");
+
+  var tvResult = response.results;
+
+  for (var i = 0; i < tvResult.length; i++) {
+          if (tvResult[i].original_language == "en" && tvResult[i].popularity >= 100 && tvResult[i].vote_average >=7) {
+            var tvDiv = $("#tvResults");
+            var title = tvResult[i].original_name;
+            var plot = tvResult[i].overview;
+            var date = tvResult[i].first_air_date;
+            var tvIMG = tvResult[i].poster_path;
+            var tvimgURL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2" + tvIMG;
+//            var image = $("<img>").attr("src", imgURL);
+
+//Grab the table from the HTML and put our movie data in it!  
+            var table = document.getElementById("tvData");
+            var row = table.insertRow(0);
+
+              var posterCell = row.insertCell(0);
+              var descCell = row.insertCell(1);
+
+                posterCell.innerHTML = ("<img src=" + tvimgURL + ">");
+                descCell.innerHTML = ("<h3>" + title + "</h3><br><h4>" + date + "</h4><p>" + plot + "</p><hr>");
+
+          };
+  }
+})
